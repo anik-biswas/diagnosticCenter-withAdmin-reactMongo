b15215177950fccd2882ddaebe539da68a9b1e23
anik-biswas <anik.biswas.an@gmail.com>
2023-11-29 08:14:58 +0600

    About page set with Mui

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,16 +1,29 @@
-import { Button, Typography } from '@mui/material';
+import { Button, Container, Paper, Typography } from '@mui/material';
 import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <Typography variant="h1" color="primary">
-        Welcome to My App
-      </Typography>
-      <Button variant="contained" color="secondary">
-        Click Me
-      </Button>
-        </div>
+        <Container>
+      <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+        <Typography variant="h4" gutterBottom>
+          About Us
+        </Typography>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo nec enim interdum, vel scelerisque
+          lacus hendrerit. Phasellus non sagittis justo. Nullam facilisis sodales sagittis.
+        </Typography>
+        <Typography paragraph>
+          Nunc nec eleifend libero. Morbi feugiat sapien sit amet nunc sollicitudin scelerisque. Nullam at risus a orci
+          aliquet luctus. Proin consequat euismod metus, vel vestibulum metus hendrerit in. Duis hendrerit arcu eget
+          elit accumsan hendrerit. Sed auctor mi vel lorem blandit, id varius neque convallis.
+        </Typography>
+        <Typography paragraph>
+          Etiam euismod augue ut justo feugiat, a eleifend elit malesuada. Fusce non venenatis ligula. Ut ac mi nec
+          purus cursus suscipit id ac arcu. Nullam commodo tellus vitae nulla consequat aliquam. Ut rhoncus tortor vel
+          fermentum vestibulum.
+        </Typography>
+      </Paper>
+    </Container>
     );
 };
 
